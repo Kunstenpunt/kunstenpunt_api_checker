feb73d36a16d9e41927e518f13547c25bf1528fa
Tom Ruette <tom.ruette@gmail.com>
2017-03-24 09:05:42 +0100

    empty received responses and diff folder + minor changes in test cases

--- kunstenpunt_api_checker.py
+++ kunstenpunt_api_checker.py
@@ -2,6 +2,8 @@ from requests import get
 from json import load, loads, dumps, decoder
 from jsondiff import diff
 from codecs import open
+from os import remove
+from glob import glob
 
 # assuming 10to1 will do tests to validate api output against swagger defined json schema,
 # these are "semantic" tests to see if output corresponds to what we expect
@@ -18,12 +20,12 @@ tests = {
     "organiteiten_met_beginletter_en_active": "/organiteiten?organiteit_begint_met=a&active=True&offset=0&limit=1",  # lijst van actieve organiteiten die beginnen met a
     "organiteiten_met_beginletter_genre_en_active": "/organiteiten?organiteit_begint_met=a&genre_id=[251]&active=True&offset=0&limit=1",  # lijst van actieve organiteiten die beginnen met a en een genre hebben
     "organiteit_jan_decleir": "/organiteiten/211878826",  # een organiteit persoon
-    "organiteit_kaaitheater": "/organiteiten/22.......",  # een organiteit organisatie
+    "organiteit_kaaitheater": "/organiteiten/22360690",  # een organiteit organisatie
     "tentoonstellingen_in_jaar": "/tentoonstellingen?jaar=2016&offset=0&limit=1",  # tentoonstellingen in een jaar
     "tentoonstellingen_in_jaar_en_maand": "/tentoonstellingen?jaar=2016&maand=12&offset=0&limit=1",  # tentoonstellingen in een jaar en een maand
     "tentoonstellingen_in_land": "/tentoonstellingen?land_id=2112986&offset=0&limit=1",  # lijst van tentoonstellingen in een land
-    "tentoonstellingen_van_kunstenaar": "/tentoonstellingen?organiteit_id=211234&offset=0&limit=1",  # lijst van tentoonstellingen van een kunstenaar
-    "tentoonstellingen_aan_organisatie": "/tentoonstellingen?organiteit_id=221234&offset=0&limit=1",  # lijst van tentoonstellingen die aan een organisatie hangen
+    "tentoonstellingen_van_kunstenaar": "/tentoonstellingen?organiteit_id=211906274&offset=0&limit=1",  # lijst van tentoonstellingen van een kunstenaar
+    "tentoonstellingen_aan_organisatie": "/tentoonstellingen?organiteit_id=22377206&offset=0&limit=1",  # lijst van tentoonstellingen die aan een organisatie hangen
     "tentoonstelling": "/tentoonstellingen/29457355",  # specifieke tentoonstelling
     "recent_gewijzigde_activiteiten": "/recent_gewijzigde_activiteiten",
     "recent_gewijzigde_buitenlandse_concerten": "/recent_gewijzigde_activiteiten/buitenlandse_concerten",
@@ -31,23 +33,29 @@ tests = {
     "binnenkort_podiumvoorstellingen": "/binnenkort/podiumvoorstellingen",
     "tien_jaar_geleden": "/tien_jaar_geleden",
     "bcs": "/buitenlandse_concerten?offset=0&limit=1",  # lijst van buitenlandse concerten
-    "bc_in_jaar": "/buitenlandse_concerten?jaar=2016&offset=0&limit=1",
+    "bc_in_jaar": "/buitenlandse_concerten?jaar=2016&offset=0&limit=5",
     "bc_in_land": "/buitenlandse_concerten?land_id=12928&offset=0&limit=1",
     "bc_in_stad": "/buitenlandse_concerten?location_id=4778&offset=0&limit=1",
-    "bc_door_mcv_artist": "/buitenlandse_concerten?organiteit_id=212345346&offset=0&limit=1",
-    "bc_door_dkb_artist": "/buitenlandse_concerten?organiteit_id=112345&offset=0&limit=1",
+    "bc_door_mcv_artist": "/buitenlandse_concerten?organiteit_id=21154637&offset=0&limit=1",
+    "bc_door_dkb_artist": "/buitenlandse_concerten?organiteit_id=211950063&offset=0&limit=1",
     "bc_in_periode": "/buitenlandse_concerten?from_date=01/01/2017&until_date=31/01/2017&offset=0&limit=1",
     "bc_in_periode_en_land": "/buitenlandse_concerten?country_id=12928from_date=01/01/2017&until_date=31/01/2017&offset=0&limit=1",
     "bc": "/buitenlandse_concerten/2112345",  # een specifiek buitenlands concert
     "podiumproducties_in_seizoen": "/podiumproducties?seizoen_id=123451&offset=0&limit=1",
-    "podiumproducties_met_genre": "/podiumproducties?genre_id=[1234,1235]&offset=0&limit=1",
+    "podiumproducties_met_genre": "/podiumproducties?genre_id=[272301]&offset=0&limit=1",
     "podiumproducties_met_beginletter": "/podiumproducties?activiteit_naam_begint_met=a&offset=0&limit=1",
     "podiumproducties_van_organiteit": "/podiumproducties?organiteit_id=211878826&offset=0&limit=1",
-    "podiumproducties_die_gelinkt_zijn_aan_andere_producties": "/podiumproducties?podium_productie_id=213456&offset=0&limit=1",
-    "podiumproducties_in_seizoen_met_genre": "/podiumproducties?seizoen_id=1223451&genre_id=[1234,12345]&offset=0&limit=1",
-    "podiumproducties_in_seizoen_van_organiteit": "/podiumproducties?seizoen_id=12344&organiteit_id=211878826&offset=0&limit=1",
+    "podiumproducties_die_gelinkt_zijn_aan_andere_producties": "/podiumproducties?podium_productie_id=452507&offset=0&limit=10",
+    "podiumproducties_in_seizoen_met_genre": "/podiumproducties?seizoen_id=1712&genre_id=[272301]&offset=0&limit=1",
+    "podiumproducties_in_seizoen_van_organiteit": "/podiumproducties?seizoen_id=1712&organiteit_id=211878826&offset=0&limit=3",
     "podiumproductie_welwillenden": "/podiumproducties/452507"
 }
+
+for f in glob("received_responses/*"):
+    remove(f)
+
+for f in glob("diffs/*"):
+    remove(f)
 
 diffs = {}
 for test in tests:
